0e6a0fbed7778f9af2bd4df29240b23f65a95b2e
mbassy199 <awuleynovember@gmail.com>
2024-12-30 20:22:59 +0000

    update

--- backend/settings.py
+++ backend/settings.py
@@ -106,12 +106,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'tkkLJTVpkhyjmZryGZIIEzuRgjJWRWPz',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '44124',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
